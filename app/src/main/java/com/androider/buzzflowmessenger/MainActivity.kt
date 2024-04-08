d6e6230f8664d10f7042879137f0f95fe3c75e4f
Kanat Bayalbayev <Kanatkz07@mail.ru>
2024-04-08 14:49:48 +0500

    Initial commit

---
+++ app/src/main/java/com/androider/buzzflowmessenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.androider.buzzflowmessenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
