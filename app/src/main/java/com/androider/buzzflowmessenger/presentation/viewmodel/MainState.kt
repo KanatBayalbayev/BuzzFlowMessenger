4df7e134c3e10e3ce627fa22cc4866a06353731b
Kanat Bayalbayev <kanatdev0709@gmail.com>
2024-05-25 18:47:13 +0500

    refactored MainState

--- app/src/main/java/com/androider/buzzflowmessenger/presentation/viewmodel/MainState.kt
+++ app/src/main/java/com/androider/buzzflowmessenger/presentation/viewmodel/MainState.kt
@@ -4,11 +4,11 @@ import com.androider.buzzflowmessenger.domain.models.CurrentUserEntity
 import com.androider.buzzflowmessenger.domain.models.FoundUserEntity
 import com.androider.buzzflowmessenger.domain.models.MessageEntity
 
-sealed class MainState{
-    object Loading : MainState()
-    data class Success(val user: FoundUserEntity?) : MainState()
-    data class CurrentUser(val currentUser: CurrentUserEntity?) : MainState()
-    data class Chats(val chats: ArrayList<FoundUserEntity>?) : MainState()
-    data class Messages(val messages: ArrayList<MessageEntity>?) : MainState()
-    data class Error(val exception: String?) : MainState()
+sealed interface MainState{
+    object Loading : MainState
+    data class Success(val user: FoundUserEntity?) : MainState
+    data class CurrentUser(val currentUser: CurrentUserEntity?) : MainState
+    data class Chats(val chats: ArrayList<FoundUserEntity>?) : MainState
+    data class Messages(val messages: ArrayList<MessageEntity>?) : MainState
+    data class Error(val exception: String?) : MainState
 }
